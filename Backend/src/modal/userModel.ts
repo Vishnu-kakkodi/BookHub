a6270b9a10c2d1c13a3454728cb4f54af53e9bf1
Vishnu P <pvishnukakkodi@gmail.com>
2025-01-30 10:57:45 +0530

    Elastic Search implemented

--- Backend/src/modal/userModel.ts
+++ Backend/src/modal/userModel.ts
@@ -1,14 +1,14 @@
 import { Schema, model } from 'mongoose';
-import { IUserDocument, UserStatus } from '../types/userType';
+import { IUser, IUserDocument } from '../types/userType';
 
 const userSchema = new Schema<IUserDocument>(
-  {
-    userName: {type: String, required: true},
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    profilePhoto: { type: String, required: false },
-  },
-  { timestamps: true }
+    {
+        userName: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        profilePhoto: { type: String, required: false },
+    },
+    { timestamps: true }
 );
 
 export const UserModel = model<IUserDocument>('User', userSchema);
