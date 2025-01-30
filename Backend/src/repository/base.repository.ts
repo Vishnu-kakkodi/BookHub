3b23051515c1bc664e971fd605dd0016ef1917f7
Vishnu P <pvishnukakkodi@gmail.com>
2025-01-30 10:15:40 +0530

    Elastic Search implemented

--- Backend/src/repository/base.repository.ts
+++ Backend/src/repository/base.repository.ts
@@ -1,12 +1,12 @@
-import { Model, Document, Types, FilterQuery } from "mongoose";
+import { Model, HydratedDocument, Types, FilterQuery } from "mongoose";
 import { BaseInterface } from "../types/baseType";
 import { HttpException } from "../middleware/error.middleware";
 import STATUS_CODES from "../constants/statusCode";
 import MESSAGES from "../constants/message";
 import { IBaseRepository } from "../interfaces/IBaseRepository";
 
-export abstract class BaseRepository<T extends BaseInterface & Document> implements IBaseRepository<T>{
-    constructor(protected readonly model: Model<Document & T>) {}
+export abstract class BaseRepository<T extends BaseInterface> implements IBaseRepository<T>{
+    constructor(protected readonly model: Model<HydratedDocument<T>>) {}
 
     async create(data: Partial<T>): Promise<T> {
         try {
